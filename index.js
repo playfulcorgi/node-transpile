dddbf75085b65ae53bf3f413336724d37902e525
playfulcorgi <unrulybeardedweekend@gmail.com>
2018-05-25 17:27:17 +0200

    Move Webpack plugins into separate directory

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 const parseArgs = require('minimist')(process.argv.slice(2))
-const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages')
-const ProgressBarPlugin = require('progress-bar-webpack-plugin')
-const chalk = require('chalk')
 const shouldWatch = parseArgs._[0] === 'watch'
 const webpack = require('webpack')
 const { resolve } = require('path')
+const PrettyStats = require('./webpack-plugins/PrettyStats')
+const ProgressBar = require('./webpack-plugins/ProgressBar')
 
 const baseDirectoryPath = process.cwd()
 const packagePath = __dirname
@@ -18,32 +17,8 @@ const compiler = webpack({
   watch: shouldWatch,
   context: baseDirectoryPath,
   plugins: [
-    new ProgressBarPlugin({
-      format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
-      clear: false
-    }),
-    {
-      apply(compiler) {
-        compiler.hooks.invalid.tap('compilingMessagePlugin', () => { console.log('Compiling...') })
-        compiler.hooks.done.tap('compilingMessagePlugin', (stats) => {
-            var rawMessages = stats.toJson({}, true)
-            var messages = formatWebpackMessages(rawMessages)
-            if (!messages.errors.length && !messages.warnings.length) {
-              console.log('Compiled successfully!')
-            }
-            if (messages.errors.length) {
-              console.log('Failed to compile.')
-              messages.errors.forEach(e => console.log(e))
-              return
-            }
-            if (messages.warnings.length) {
-              console.log('Compiled with warnings.')
-              messages.warnings.forEach(w => console.log(w))
-            }
-          }
-        )
-      }
-    }
+    new ProgressBar(),
+    new PrettyStats()
   ],
   externals: [
     nodeExternals()
---
+++ webpack-plugins/ProgressBar.js
@@ -0,0 +1,7 @@
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const chalk = require('chalk')
+
+module.exports = () => new ProgressBarPlugin({
+  format: `  build [:bar] ${chalk.green.bold(':percent')} (:elapsed seconds)`,
+  clear: false
+})
